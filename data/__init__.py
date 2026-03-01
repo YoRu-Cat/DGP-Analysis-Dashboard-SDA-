92936fa2e1665c4121a374d28ce70f53c0ed58dd
YoRu <tech.niko.coding@gmail.com>
2026-03-01 19:04:42 +0500

    Version 2.4.2 (tiny bits of changes)

--- data/__init__.py
+++ data/__init__.py
@@ -1,2 +1,1 @@
-# Data Package
-# Contains source data files (CSV, JSON, Excel) used by the input plugins.
+
--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,6 +1,1 @@
-# Plugins Module
-# Contains Input (Source) and Output (Sink) implementations.
-# Input plugins interact only via PipelineService protocol defined in Core.
-# Output plugins satisfy the DataSink protocol defined in Core.
-
 __all__ = ['inputs', 'outputs']
